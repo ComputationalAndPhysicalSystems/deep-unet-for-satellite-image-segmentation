36d6aba3af56ecfb65911c4243e1e8c3944eddef
morphogenetics <33558091+morphogenetics@users.noreply.github.com>
2019-04-29 14:45:08 -0700

    patches form coherent image

--- predict.py
+++ predict.py
@@ -13,6 +13,7 @@ def predict(x, model, patch_sz=160, n_classes=5):
     # make extended img so that it contains integer number of patches
     npatches_vertical = math.ceil(img_height / patch_sz)
     npatches_horizontal = math.ceil(img_width / patch_sz)
+    print("vert", npatches_vertical, "horizontal",npatches_horizontal) 
     extended_height = patch_sz * npatches_vertical
     extended_width = patch_sz * npatches_horizontal
     ext_x = np.zeros(shape=(extended_height, extended_width, n_channels), dtype=np.float32)
@@ -32,15 +33,23 @@ def predict(x, model, patch_sz=160, n_classes=5):
             patches_list.append(ext_x[x0:x1, y0:y1, :])
     # model.predict() needs numpy array rather than a list
     patches_array = np.asarray(patches_list)
+    
     # predictions:
     patches_predict = model.predict(patches_array, batch_size=4)
     prediction = np.zeros(shape=(extended_height, extended_width, n_classes), dtype=np.float32)
-    for k in range(patches_predict.shape[0]):
-        i = k // npatches_horizontal
-        j = k % npatches_vertical
+    k = 0 
+    for i in range(0, npatches_vertical):
+        
+        #i = k // npatches_horizontal
         x0, x1 = i * patch_sz, (i + 1) * patch_sz
-        y0, y1 = j * patch_sz, (j + 1) * patch_sz
-        prediction[x0:x1, y0:y1, :] = patches_predict[k, :, :, :]
+        
+        for j in range(0, npatches_horizontal):
+        
+            y0, y1 = j * patch_sz, (j + 1) * patch_sz
+            #print(x0,x1,y0,y1)
+            prediction[x0:x1, y0:y1, :] = patches_predict[k, :, :, :]
+            k += 1
+            print(k)
     return prediction[:img_height, :img_width, :]
 
 
